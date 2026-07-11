da7f6014e12870d1946f3ce4109ad8edfc223891
Aniket Gupta <167573220+anikegupta@users.noreply.github.com>
2026-07-11 21:21:56 +0530

    Updated CV

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -87,7 +87,7 @@ export const Hero = () => {
               <Button size="lg" href="#contact">
                 Contact Me <ArrowRight className="w-5 h-5" />
               </Button>
-              <a href="https://drive.google.com/file/d/1SVb2H071Zic4tdJKleYp4evEFFJZ4O-0/view"  target="_blank">
+              <a href="https://drive.google.com/file/d/1k1EXDtJmfcJM-cCOW0lYU0AYWbOkwBid/view"  target="_blank">
               <AnimatedBorderButton>
                 
                 Download CV
